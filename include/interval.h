ac83e19eb73af9aec793dad68fc848b6ce4b44dc
Tkachenko Leonid Urievich <tkachenkoli@tesla-cmc.cmc.msu.ru>
2017-10-09 07:13:40 +0300

    Copy project

--- include/interval.h
+++ include/interval.h
@@ -1,14 +1,36 @@
-#ifndef INTERVAL_H
-#define INTERVAL_H
+#pragma once
 
 #include <iostream>
 #include <stdio.h>
+#include <exception>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 #define DEVICE 0
 #define MAX_NUM_RUNS (100)
 
+/*
+struct Interval {
+    double bottom;
+    double up;   
+}; 
+ 
 
+struct Box {
+    Interval *i;
+    Interval funBounds;
+    double funValue;
+    
+    Box(int inRank) {
+        i = new Interval[inRank];
+    }
+    ~Box(){
+        delete [] i;
+    }
+};
 
+ * */
 
 int const BLOCK_SIZE = 128;
 
@@ -23,4 +45,4 @@ int const BLOCK_SIZE = 128;
         }                                                      \
     } while (0)
 
-#endif
+
